c4953412f404b6ef112736210d0f41bfdc32055c
Guido2023 <gdf190@gmail.com>
2025-02-23 21:15:42 -0500

    Proyecto-django-2

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path('create/', BlogCreateView.as_view(), name='create'),
+    path('<int:id>/', BlogDetailView.as_view(), name='detail'),
+    path('<int:id>/update/', BlogUpdateView.as_view(), name='update'),
+    path('<int:id>/delete/', BlogDeleteView.as_view(), name='delete'),
 ]
